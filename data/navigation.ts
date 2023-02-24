8a799fdb84c0203a49c53690627ca3647921f1a3
Mainak Das <mainakdas104@gmail.com>
2023-02-25 00:34:16 +0530

    Feat: Add navigation data

---
+++ data/navigation.ts
@@ -0,0 +1,12 @@
+export const navigationLinks = [
+  { id: 1, name: 'Home', href: '/' },
+  { id: 2, name: 'About', href: '/about' },
+  { id: 3, name: 'Work', href: '/our-work' },
+  { id: 4, name: 'Blogs', href: '/blogs' },
+  { id: 5, name: 'Technologies', href: '/techs' },
+  { id: 6, name: 'Contact Us', href: '/contact' },
+  // { id: 2, name: 'Work', href: '/our-work' },
+  // { id: 2, name: 'Work', href: '/our-work' },
+  // { id: 2, name: 'Work', href: '/our-work' },
+  // { id: 2, name: 'Work', href: '/our-work' },
+];
